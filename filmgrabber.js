1cc1576e6afcc6e39e4f4c4e36231b8b1cd4cee2
Joao Paulo <joaoded@hotmail.com>
2017-04-10 23:22:37 -0300

    Implementing some comments in the code

--- filmgrabber.js
+++ filmgrabber.js
@@ -2,11 +2,16 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 request('http://www.imdb.com/chart/moviemeter', function(err, res, body){
+	// identificando erros
 	if (err) console.log('Erro: ' +err);
 
+	//pegando body
 	var $ = cheerio.load(body);
 
 	$('.lister-list tr').each(function(){
+
+		//pegando as informacoes que interessam
+
 		var title = $(this).find('.titleColumn a').text().trim();
 		var rating = $(this).find('.imdRating strong').text().trim();
 

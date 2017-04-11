f5a0806c165cd51ecc2cd00887f39382442c552a
Joao Paulo <joaoded@hotmail.com>
2017-04-10 23:31:11 -0300

    More inclusions

--- filmgrabber.js
+++ filmgrabber.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require ('fs');
 
 request('http://www.imdb.com/chart/moviemeter', function(err, res, body){
 	// identificando erros
@@ -16,6 +17,8 @@ request('http://www.imdb.com/chart/moviemeter', function(err, res, body){
 		var rating = $(this).find('.imdRating strong').text().trim();
 
 		console.log('Titulo: ' +title);
+
+		fs.appendFile('imdb.txt', title + ' ' + rating + '\n');
 		
 
 	});
